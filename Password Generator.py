adb978bca798495a4d27d95372efdeaa4909606d
Abu <138589928+abusiddu65@users.noreply.github.com>
2023-12-30 16:55:00 +0530

    Delete Password Generator.py

--- Password Generator.py
+++
@@ -1,9 +0,0 @@
-import random 
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+=!@#$%^&*"
-length = int(input("Enter the length of the password: "))
-def password_generator(num):
-    password = ''
-    for i in range(num):
-        password += random.choice(characters)
-    return password
-print(password_generator(length))   
